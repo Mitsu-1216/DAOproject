7ec68a35b70ca09c69f04aa6d6bec58eb998f69e
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-07-12 07:02:22 +0900

    second

--- DAOproject/src/main/java/SampleDAO.java
+++ DAOproject/src/main/java/SampleDAO.java
@@ -1,6 +1,20 @@
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 
 public class SampleDAO {
-
+public static void main(String[] args) throws SQLException {
+//データベースに接続
+	try (Connection conn = DriverManager.getConnection(
+			"jdbc:h2:tcp://localhist/-/example","sa","")){
+		
+		//SQL文生成
+		String sql;
+	}
+	
+	
+}
 }
