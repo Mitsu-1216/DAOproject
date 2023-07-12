e667c16168ade223f38cf6ab6f17dfba96414b15
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-07-13 08:37:56 +0900

    SQL

--- DAOproject/src/main/java/SampleDAO.java
+++ DAOproject/src/main/java/SampleDAO.java
@@ -14,6 +14,14 @@ public static void main(String[] args) throws SQLException {
 		//SQL文生成
 		String sql="SELECT ID,NAME,AGE FROM EMPLOYE";
 		PreparedStatement pStmt = conn.prepareStatement(sql);
+		
+		ResultSet rs = pStmt.executeQuery();
+		
+		while (rs.next()) {
+			String id = rs.getString("ID");
+			String name = rs.getString("NAME");
+			int age = rs.getInt("AGE");
+		}
 	}
 	
 	
