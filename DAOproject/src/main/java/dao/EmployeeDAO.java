79a3cc43f48311d767fd0f78c5a9e1c0998e889d
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-12-05 08:32:02 +0900

    DAO

---
+++ DAOproject/src/main/java/dao/EmployeeDAO.java
@@ -0,0 +1,36 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class EmployeeDAO {
+public static void main(String[] args) throws SQLException {
+//データベースに接続
+	try (Connection conn = DriverManager.getConnection(
+			"jdbc:mysql:tcp://localhost/~/example","sa","")){
+		
+		//SQL文生成
+		String sql="SELECT ID,NAME,AGE FROM EMPLOYE";
+		PreparedStatement pStmt = conn.prepareStatement(sql);
+		
+		ResultSet rs = pStmt.executeQuery();
+		
+		while (rs.next()) {
+			String id = rs.getString("ID");
+			String name = rs.getString("NAME");
+			int age = rs.getInt("AGE");
+			
+			System.out.println(id);
+			System.out.println(name);
+			System.out.println(age);
+		}
+	}
+		catch (SQLException e) {
+			e.printStackTrace();
+			}
+	}
+}
